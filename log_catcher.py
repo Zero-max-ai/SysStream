2d3ad2b1d8b5a3ecc25c6d0f7a3a5861c4452e0c
Zero-max-ai <gangi.keshav03@gmail.com>
2025-09-03 17:05:28 +0530

    added log catcher with file generation and updated README.md

---
+++ log_catcher.py
@@ -0,0 +1,26 @@
+import psutil, time, json
+from datetime import datetime
+from pathlib import Path
+
+# init path
+log_dir = Path('logs')
+log_dir.mkdir(exist_ok=True)
+
+"""
+    creating file for daily basis,
+    storing events,
+    appending data in the file,
+"""
+while True:
+    log_file = log_dir / f"logs_{datetime.now().date()}.json"
+    event = {
+        "timeStamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "cpu": psutil.cpu_percent(),
+        "memory": psutil.virtual_memory().percent,
+        "disk": psutil.disk_usage('/').percent,
+        "top_processes": [p.info for p in psutil.process_iter(['pid', 'name', 'cpu_percent'])][:5]
+    }
+
+    with open(log_file, "a") as f:
+        f.write(json.dumps(event) + ',\n')
+    time.sleep(10) # placing interval
